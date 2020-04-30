31181787abf5640db2aa10a1bbb22dc90f8b7b72
Evleen <evleensingh1313@gmail.com>
2020-04-30 23:26:48 +0530

    Added Number to words code

---
+++ Week3/NumToWords/src/com/sapient/week3/NumToWords.java
@@ -0,0 +1,67 @@
+package com.sapient.week3;
+
+import java.util.Scanner;
+
+public class NumToWords {
+	public static void main(String[] args) {
+		int number = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please type a number:");
+		number = scanner.nextInt();
+		if (number == 0) {
+			System.out.print("Number in words: Zero");
+		} else {
+			System.out.print("Number in words: " + numberToWord(number));
+		}
+
+		scanner.close();
+	}
+
+	private static String numberToWord(int number) {
+		// TODO Auto-generated method stub
+		String words = "";
+		String unitsArray[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+				"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+		String tensArray[] = { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty",
+				"ninety" };
+
+		if (number == 0) {
+			return "zero";
+		}
+		// add minus before conversion if the number is less than 0
+		if (number < 0) {
+			
+			String numberStr = "" + number;
+			
+			numberStr = numberStr.substring(1);
+			return "minus " + numberToWord(Integer.parseInt(numberStr));
+		}
+		if ((number / 100000) > 0) {
+			words += numberToWord(number / 100000) + " lakh ";
+			number %= 100000;
+		}
+		
+		if ((number / 1000) > 0) {
+			words += numberToWord(number / 1000) + " thousand ";
+			number %= 1000;
+		}
+		
+		if ((number / 100) > 0) {
+			words += numberToWord(number / 100) + " hundred ";
+			number %= 100;
+		}
+
+		if (number > 0) {
+			if (number < 20) {
+				// fetch the appropriate value from unit array
+				words += unitsArray[number];
+			} else {
+				words += tensArray[number / 10];
+				if ((number % 10) > 0) {
+					words += "-" + unitsArray[number % 10];
+				}
+			}
+		}
+		return words;
+	}
+}
