fe58d23e27b19c7dd91c46889e56267d262cce00
Evleen <evleensingh1313@gmail.com>
2020-06-13 12:33:54 +0530

    Added week10 assignment initial commit

---
+++ week10/src/main/java/com/sapient/week10/EmployeeDaoService.java
@@ -0,0 +1,48 @@
+package com.sapient.week10;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class EmployeeDaoService {
+	private static List<Employee> employees = new ArrayList<>();
+	private static int employeeCount = 3;
+	static {
+		employees.add(new Employee(1,"Eve",new Date()));
+		employees.add(new Employee(2,"Jack",new Date()));
+		employees.add(new Employee(3,"lol",new Date()));
+	}
+	public List<Employee> findAll() {
+		return employees;
+	}
+
+	public Employee save(Employee employee) {
+		if (employee.getId() == 0) {
+			employee.setId(++employeeCount);
+		}
+		employees.add(employee);
+		return employee;
+	}
+	
+
+	public Employee findOne(int id) {
+		for (Employee employee : employees) {
+			if (employee.getId() == id) {
+				return employee;
+			}
+		}
+		return null;
+	}
+	public Employee findByName(String name) {
+		for (Employee employee : employees) {
+			if ((employee.getName()).equals(name)) {
+				return employee;
+			}
+		}
+		return null;
+	}
+}
---
+++ week10/src/main/java/com/sapient/week10/EmployeeResource.java
@@ -0,0 +1,45 @@
+package com.sapient.week10;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class EmployeeResource {
+	@Autowired
+	private EmployeeDaoService service;
+
+	@GetMapping("/employees")
+	public List<Employee> retrieveAllUsers() {
+		return service.findAll();
+	}
+
+	@GetMapping("/employees/{id}")
+	public Employee retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
+
+	@GetMapping("/employee/{name}")
+	public Employee retrieveUserByName(@PathVariable String name) {
+		return service.findByName(name);
+	}
+
+	@PostMapping("/employees")
+	public ResponseEntity<Object> createUser(@RequestBody Employee employee) {
+		Employee Saveduser = service.save(employee);
+		URI location = ServletUriComponentsBuilder.
+				fromCurrentRequest().path("/{id}").buildAndExpand(Saveduser.getId())
+				.toUri();
+		return ResponseEntity.created(location).build();
+	}
+}
