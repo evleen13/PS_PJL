d2d2046c251edfdd43ed9d353e0e8ab3c84d5c63
Evleen <evleensingh1313@gmail.com>
2020-06-16 15:42:37 +0530

    Minor changes to DaoService file

--- week10/src/main/java/com/sapient/week10/EmployeeDaoService.java
+++ week10/src/main/java/com/sapient/week10/EmployeeDaoService.java
@@ -6,16 +6,16 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class EmployeeDaoService {
 	private static List<Employee> employees = new ArrayList<>();
 	private static int employeeCount = 3;
 	static {
-		employees.add(new Employee(1,"Eve",new Date()));
-		employees.add(new Employee(2,"Jack",new Date()));
-		employees.add(new Employee(3,"lol",new Date()));
+		employees.add(new Employee(1, "Eve", new Date()));
+		employees.add(new Employee(2, "Jack", new Date()));
+		employees.add(new Employee(3, "lol", new Date()));
 	}
+
 	public List<Employee> findAll() {
 		return employees;
 	}
@@ -27,7 +27,6 @@ public class EmployeeDaoService {
 		employees.add(employee);
 		return employee;
 	}
-	
 
 	public Employee findOne(int id) {
 		for (Employee employee : employees) {
@@ -37,6 +36,7 @@ public class EmployeeDaoService {
 		}
 		return null;
 	}
+
 	public Employee findByName(String name) {
 		for (Employee employee : employees) {
 			if ((employee.getName()).equals(name)) {
@@ -44,5 +44,28 @@ public class EmployeeDaoService {
 			}
 		}
 		return null;
+	}
+
+	public void delete(int id) {
+
+		for (Employee e : employees) {
+			if (e.getId() == id) {
+
+				e = null;
+				break;
+			}
+		}
+	}
+
+	public void modify(int id, Employee employee) {
+
+		for (Employee e : employees) {
+			if (e.getId() == id) {
+
+				e.setName(employee.getName());
+				e.setBirthDate(employee.getBirthDate());
+				break;
+			}
+		}
 	}
 }
--- week10/src/main/java/com/sapient/week10/EmployeeResource.java
+++ week10/src/main/java/com/sapient/week10/EmployeeResource.java
@@ -7,9 +7,11 @@ import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -41,5 +43,16 @@ public class EmployeeResource {
 				fromCurrentRequest().path("/{id}").buildAndExpand(Saveduser.getId())
 				.toUri();
 		return ResponseEntity.created(location).build();
+	}
+	@DeleteMapping("/employees/{id}")
+	public void deleteEmployee(@PathVariable int id) {
+		
+		service.delete(id);
+	}
+	
+	@PutMapping("/employees/{id}")
+	public void modifyEmployee(@PathVariable int id, @RequestBody Employee e ) {
+	
+		service.modify(id, e);
 	}
 }
